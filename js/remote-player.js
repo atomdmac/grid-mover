e30457fd67835be87cec34642059a95f41134e76
suicidepills <atom.mac@gmail.com>
2012-09-22 17:12:15 -0400

    Clean-up, comments, 'click' debug tool for remotes.
    Clicking on a remote mover will now print information about that mover.
    This includes any data that was passed along with the ANNOUNCE_MOVERS
    or JOIN event.

--- js/remote-player.js
+++ js/remote-player.js
@@ -2,13 +2,16 @@
  * Represents a non-local player.
  */
 Crafty.c("RemoteMover", {
-
+	
+	// Data describing this mover.  Passed from server.
+	_data: null,
+	
 	// Path to follow.
 	_path: null,
 	
 	// Update position based on new info from server.
 	updatePosition: function (data) {
-		
+		// !!! DEBUG !!! //
 		console.log("Remote Movement: ", data.direction);
 		
 		// Stop all current tweens on this.
@@ -35,12 +38,11 @@ Crafty.c("RemoteMover", {
 	
 	// NOTE: We use position here, not direction.  GET IT?
 	_move: function (position) {
-		console.log("move to ", position);
 		this.tween(position, this._moveDuration);
 	},
 	
+	// Called when the _move() animation finishes.
 	_onTileArrive: function (e) {
-		console.log("ARRIVAL!");
 		this._isMoving = false;
 		
 		// Tell everyone about it!
@@ -55,10 +57,8 @@ Crafty.c("RemoteMover", {
 		this.trigger("ChangeCellEnd", event);
 		
 		// Keep moving?
-		console.log("path = ", this._path);
 		if (this._path.length > 0) {
 			var next = this._path.pop();
-			console.log("next = ", next);
 			this._move(next);
 		}
 	},
@@ -68,12 +68,17 @@ Crafty.c("RemoteMover", {
 		
 		// !!! DEBUG !!! //
 		console.log("Creating Remote Mover: ", data);
-	
+		
+		// Cache data in case we need it later.
+		this._data = data;
+		
+		// Save ID info.
 		this.id = data.id;
+		
+		// Convert the given cell/grid position to real coordinates.
 		var pos = this._toCoords(data);
 		
-		console.log("POS = ", pos);
-		
+		// Apply data to our remote mover instance.
 		this.attr({
 			x: pos.x,
 			y: pos.y,
@@ -100,18 +105,28 @@ Crafty.c("RemoteMover", {
 	
 	// Pre-init stuff.
 	init: function () {
-		// HACK ATTACK
+		// Used for scoping.
+		var self = this;
+	
+		// HACK ATTACK: Make sure the functions defined here don't get overriden
+		// by the components that we add.
 		var tileArriveTemp = this._onTileArrive;
 		var moveTemp = this._move;
 		
 		// Dependencies.
-		this.requires("2D, DOM, Color, GridMover");
+		this.requires("2D, DOM, Color, GridMover, Mouse");
 		
-		// THIS IS A TOTAL HACK:
+		// THIS IS A TOTAL HACK: (Continued from HACK ATTACK above).
 		this._onTileArrive = tileArriveTemp;
 		this._move = moveTemp;
 		
 		// Initialize path array.
 		this._path = [];
+		
+		// !!! DEBUG !!! //
+		// When this entity is clicked, print information about it.
+		this.bind("Click", function (e) {
+			console.log("Remote Player Info: ", this._data);
+		});
 	}
 });
